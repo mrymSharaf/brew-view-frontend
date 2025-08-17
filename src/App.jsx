7ad34f624e550669681e46e47df81cca3643dac4
Mariam Sharaf <maryamshaider25@gmail.com>
2025-08-17 11:38:02 +0300

    move components to src

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router"
-import NavBar from "../components/NavBar/NavBar"
-import Footer from "../components/Footer/Footer"
+import NavBar from "./components/NavBar/NavBar"
+import Footer from "./components/Footer/Footer"
 
 const App = () => {
   return (
