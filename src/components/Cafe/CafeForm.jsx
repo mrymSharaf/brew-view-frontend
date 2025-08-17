b2308275a1c4bc976143d3df6ac14f14910eb819
ZahraaMohd-c <202203702@stu.uob.edu.bh>
2025-08-17 21:09:52 +0300

    cafe api

--- src/components/Cafe/CafeForm.jsx
+++ src/components/Cafe/CafeForm.jsx
@@ -1,8 +1,57 @@
-
+import { useState } from 'react'
 
 const CafeForm = () => {
+    const initalState = {
+        cafeName: '',
+        location: '',
+        cafeImage: ''
+    }
+
+    const [formDate, setformDate] = useState(initalState)
+
+    const handleChange = (event) => {
+        setformDate({ ...formDate, [event.target.name]: event.target.value })
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+
+
+    }
+
+
     return (
         <>
+            <h2>Cafe form</h2>
+            <form onSubmit={handleSubmit} enctype="multipart/form-data">
+                <label>Cafe Name:</label>
+                <input
+                    name='cafeName'
+                    id='cafeName'
+                    value={formDate.cafeName}
+                    onChange={handleChange}
+
+                />
+                <label>Locatin:</label>
+                <input
+                    name='location'
+                    id='location'
+                    value={formDate.location}
+                    onChange={handleChange}
+
+                />
+                <label>Cafe Image:</label>
+                <input
+                    name='cafeImage'
+                    id='cafeImage'
+                    value={formDate.cafeImage}
+                    onChange={handleChange}
+                    type='file'
+
+                />
+
+                <button type='submit'>Create</button>
+            </form>
 
         </>
     )
